71570a4a86db7c2a1925609537934708610f2a99
Carl.Vondrick <Carl.Vondrick@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-05-28 19:50:19 -0500

    sfSearch: updated test suite:   * moved prove/coverage to test/bin/ to follow symfony standards   * made prove/coverage testing easier for other plugins
    git-svn-id: http://svn.symfony-project.com/plugins/sfSearchPlugin/trunk@9353 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ test/bin/all.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This file is part of the sfSearch package.
+ * (c) Carl Vondrick <carl.vondrick@symfony-project.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require dirname(__FILE__) . '/suite.php';
+
+$h = new lime_search_harness(new lime_output_color);
+$h->base_dir = realpath(dirname(__FILE__) . '/../../../');
+$h->register(sfFinder::type('file')->name('xf*Test.php')->in(glob($h->base_dir . '/*/test/')));
+$h->run();
---
+++ test/bin/suite.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * This file is part of the sfSearch package.
+ * (c) Carl Vondrick <carl.vondrick@symfony-project.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once dirname(__FILE__) . '/../bootstrap/unit.php';
+require_once 'util/sfFinder.class.php';
+
+/**
+ * A class to help running tests across sfSearch
+ *
+ * @package sfSearch
+ * @subpackage Test
+ * @author Carl Vondrick
+ */
+class lime_search
+{
+  /**
+   * The root
+   *
+   * @var string
+   */
+  private $root;
+
+  /**
+   * The output
+   *
+   * @param lime_output
+   */
+  private $output;
+
+  /**
+   * Constructs a new testing system
+   *
+   * @param string $root The path to the root of the plugin
+   * @param lime_output $output The lime_output helper
+   */
+  public function __construct($root, lime_output $output)
+  {
+    $this->root = realpath($root);
+    $this->output = $output;
+  }
+
+  /**
+   * Runs prove testing on the plugin
+   */
+  public function prove()
+  {
+    $this->getHarness()->run();
+  }
+
+  /**
+   * Proves against all plugins
+   */
+  public function proveAll()
+  {
+  }
+
+  /**
+   * Runs coverage testing on the plugin
+   */
+  public function coverage($verbose = true)
+  {
+    $h = $this->getHarness();
+
+    $c = new lime_coverage($h);
+    $c->extension = '.php';
+    $c->verbose = $verbose;
+    $c->base_dir = $this->root . '/lib/';
+
+    $c->register(sfFinder::type('file')->name('*.class.php')->prune('vendor')->in($c->base_dir));
+    $c->run();
+  }
+
+  /**
+   * Gets the harness
+   *
+   * @returns lime_harnes
+   */
+  private function getHarness()
+  {
+    $h = new lime_search_harness($this->output);
+    $h->base_dir = $this->root;
+    $h->register(sfFinder::type('file')->name('xf*Test.php')->in(glob($h->base_dir . '/test/')));
+
+    return $h;
+  }
+}
+
+class lime_search_harness extends lime_harness
+{
+  protected function get_relative_file($file)
+  {
+    return preg_replace('#^.*/(.*?)Plugin/test/(unit|functional)/#', '[$1] $2/', $file);
+  }
+}
+
